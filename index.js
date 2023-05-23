956a61329fb7290da74624d7a947f826b7a6b108
KulakIlya <kulak1224@gmail.com>
2023-05-23 08:46:33 +0300

    Fixing slider bug

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ let quotesCurrentSlideNum = 0;
 
 function nextSlideStyle(nextSlideIndex) {
   const quotesSlides = document
-    .getElementById("quotes")
-    .querySelector(".slider")
-    .querySelector(".slides");
+    .getElementById('quotes')
+    .querySelector('.slider')
+    .querySelector('.slides');
 
-  const currentNextSlide = quotesSlides.querySelector(".next-slide");
-  const nextSlide = quotesSlides.querySelectorAll(".slide")[nextSlideIndex];
+  const currentNextSlide = quotesSlides.querySelector('.next-slide');
+  const nextSlide = quotesSlides.querySelectorAll('.slide')[nextSlideIndex];
 
-  nextSlide.classList.add("next-slide");
+  nextSlide.classList.add('next-slide');
 
-  currentNextSlide.classList.remove("next-slide");
+  currentNextSlide.classList.remove('next-slide');
 }
 
 function nextOrPrevSlide(sliderIndex, n) {
@@ -20,9 +20,10 @@ function nextOrPrevSlide(sliderIndex, n) {
 }
 
 function sliderBtnOnClick(sliderIndex, slideIndex) {
-  const slider = document.querySelectorAll(".slider")[sliderIndex];
+  const slider = document.querySelectorAll('.slider')[sliderIndex];
+  console.log(slider);
 
-  const sliderBtn = slider.querySelector(".slider-btn");
+  const sliderBtn = slider.querySelector('.slider-btn');
 
   if (slideIndex > Math.floor(sliderBtn.childNodes.length / 2) - 1)
     slideIndex = 0;
@@ -31,28 +32,29 @@ function sliderBtnOnClick(sliderIndex, slideIndex) {
 
   currentSlideNum = slideIndex;
 
-  const slide = slider.querySelectorAll(".slide")[slideIndex];
-  const slideBtn = sliderBtn.querySelectorAll(".slide-btn")[slideIndex];
+  const slide = slider.querySelectorAll('.slide')[slideIndex];
+  const slideBtn = sliderBtn.querySelectorAll('.slide-btn')[slideIndex];
 
-  let currentSlide = slider.querySelector(".current-slide");
+  let currentSlide = slider.querySelector('.current-slide');
 
-  let currentSlideBtn = sliderBtn.querySelector(".current-slide-btn");
+  let currentSlideBtn = sliderBtn.querySelector('.current-slide-btn');
 
-  if (!slide.classList.contains("current-slide")) {
-    slide.classList.add("current-slide");
-    currentSlide.classList.remove("current-slide");
+  if (!slide.classList.contains('current-slide')) {
+    slide.classList.add('current-slide');
+    currentSlide.classList.remove('current-slide');
 
-    slideBtn.classList.add("current-slide-btn");
-    currentSlideBtn.classList.remove("current-slide-btn");
+    slideBtn.classList.add('current-slide-btn');
+    currentSlideBtn.classList.remove('current-slide-btn');
   }
 }
 
 function accordiation(index) {
   const clickedBtn = document
-    .getElementById("questions")
-    .querySelector(".questions-list")
-    .querySelectorAll(".item")[index];
+    .getElementById('questions')
+    .querySelector('.questions-list')
+    .querySelectorAll('.item')[index];
 
-  if (!clickedBtn.classList.contains("active")) clickedBtn.classList.add("active");
-  else clickedBtn.classList.remove("active");
+  if (!clickedBtn.classList.contains('active'))
+    clickedBtn.classList.add('active');
+  else clickedBtn.classList.remove('active');
 }
